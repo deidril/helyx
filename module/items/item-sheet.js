1bbe529fdbf0030e1f424d295d1dfb8ffbd6ef30
e835012 <Frederic.Manisse@EXTERNAL.dassault-aviation.com>
2022-01-17 14:26:26 +0100

    Edition of room description

--- module/items/item-sheet.js
+++ module/items/item-sheet.js
@@ -11,10 +11,43 @@ export class HelyxItemSheet extends ItemSheet {
          */
     }
 
+    /**
+     * Extend and override the default options used by the Simple Item Sheet
+     * @returns {Object}
+     */
+    static get defaultOptions() {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+            classes: ["helyx", "sheet", "item"],
+            width: 520,
+            height: 390,
+            resizable: false,
+            tabs: [
+                {
+                    navSelector: ".tabs",
+                    contentSelector: ".sheet-body",
+                    initial: "description",
+                },
+            ],
+        });
+    }
+
     /** @override */
     get template() {
-        const path = "systems/helyx/templates/items/";
-        return `${path}/${this.item.data.type}-sheet.html`;
+        const path = "systems/helyx/templates/items";
+        const result = `${path}/${this.item.data.type}-sheet.html`;
+        console.log(result);
+        return result;
+    }
+
+    /**
+     * Prepare data for rendering the Item sheet
+     * The prepared data object contains both the actor data as well as additional sheet options
+     */
+    getData() {
+        const data = super.getData().data;
+        data.editable = this.document.sheet.isEditable;
+        data.config = CONFIG.OSE;
+        return data;
     }
 }
 // End of file
